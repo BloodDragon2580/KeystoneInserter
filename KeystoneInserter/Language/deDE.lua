695d099cf859d5cf59ed85ba7dc3316c4e8065e7
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-04-24 09:50:20 +0200

    Change
    Little changes to the translation files

--- KeystoneInserter/Language/deDE.lua
+++ KeystoneInserter/Language/deDE.lua
@@ -3,16 +3,5 @@ local myName, me = ...
 local L = me.L
 -----------------------------------------------------------
 -----------------------------------------------------------
---Für den Anfang sind alle Zauber gleich bis			 --
---ich die richtigen Sätze/Worte habe die man nutzen kann --
---Bei fehlenden Spells oder Unterbrechungen bitte auf	 --
---Github schreiben.	Am besten Spell Name und ID			 --
---In progress											 --
------------------------------------------------------------
------------------------------------------------------------
-
-
-
-
 L["OFF"] = "|cffff6666AUS|r"
 L["ON"] = "|cff66ff6eAN|r"
