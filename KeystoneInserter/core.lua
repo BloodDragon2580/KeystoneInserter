ce7f61689bcbfc79e9d4037ccd9fa44b1d63919f
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-11-16 10:19:56 +0100

    Update to v1.9
    fix auto insert

--- KeystoneInserter/core.lua
+++ KeystoneInserter/core.lua
@@ -1,26 +1,38 @@
-local InsertKeystone = function(self)
-    for bag = 0, NUM_BAG_SLOTS do
-        for slot = 1, GetContainerNumSlots(bag) do
-            local _, _, _, _, _, _, itemName = GetContainerItemInfo(bag, slot)
-            
-            if itemName and itemName:match("keystone:") then
-                PickupContainerItem(bag, slot)
-                
-                if (CursorHasItem()) then
-                    C_ChallengeMode.SlotKeystone()
-                end
-            end
-        end
-    end
-end
-
-local frame = CreateFrame("Frame", "InsertKeystone", UIParent);
-frame:RegisterEvent("ADDON_LOADED");
-
-frame:SetScript("OnEvent", function(self, event, addonName, ...)
-    if event == "ADDON_LOADED" and addonName == "Blizzard_ChallengesUI" then
-		ChallengesKeystoneFrame:HookScript("OnShow", InsertKeystone);
-        frame:UnregisterEvent("ADDON_LOADED");
-        return;
+local KeystoneInserter = CreateFrame("Frame")
+KeystoneInserter:RegisterEvent("ADDON_LOADED")
+KeystoneInserter:SetScript("OnEvent", function(self, event, addon)
+	if (addon ~= "Blizzard_ChallengesUI") then
+		return
 	end
-end);
+
+	if ChallengesKeystoneFrame then
+		local Frame = ChallengesKeystoneFrame
+		Frame:HookScript("OnShow", function()
+			local ID, ClassID, SubClassID
+
+			for bag = 0, NUM_BAG_FRAMES do
+				for slot = 1, C_Container.GetContainerNumSlots(bag) do
+					ID = C_Container.GetContainerItemID(bag, slot)
+
+					if ID then
+						ClassID, SubClassID = select(12, GetItemInfo(ID))
+
+						if (ClassID == 5 and SubClassID == 1) or (ID == 180653) then
+							return C_Container.UseContainerItem(bag, slot)
+						end
+					end
+				end
+			end
+		end)
+
+		if (not Frame:IsMovable()) then
+			Frame:SetMovable(true)
+			Frame:SetClampedToScreen(true)
+			Frame:RegisterForDrag("LeftButton")
+			Frame:SetScript("OnDragStart", Frame.StartMoving)
+			Frame:SetScript("OnDragStop", Frame.StopMovingOrSizing)
+		end
+
+		self:UnregisterEvent(event)
+	end
+end)
