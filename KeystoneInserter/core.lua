c9b7d9fceb53f57f883b5f723f3afad19fe8f09f
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-11-28 20:56:38 +0100

    Update to v2.1

--- KeystoneInserter/core.lua
+++ KeystoneInserter/core.lua
@@ -1,30 +1,24 @@
+local IDs = {[138019] = true, [151086] = true, [158923] = true, [180653] = true}
 local KeystoneInserter = CreateFrame("Frame")
 KeystoneInserter:RegisterEvent("ADDON_LOADED")
 KeystoneInserter:SetScript("OnEvent", function(self, event, addon)
-	if (addon ~= "Blizzard_ChallengesUI") then
-		return
-	end
-
+	if (addon ~= "Blizzard_ChallengesUI") then return end
 	if ChallengesKeystoneFrame then
 		local Frame = ChallengesKeystoneFrame
 		Frame:HookScript("OnShow", function()
-			local ID, ClassID, SubClassID
-
+			local ID, Class, SubClass
 			for bag = 0, NUM_BAG_FRAMES do
 				for slot = 1, C_Container.GetContainerNumSlots(bag) do
 					ID = C_Container.GetContainerItemID(bag, slot)
-
 					if ID then
-						ClassID, SubClassID = select(12, GetItemInfo(ID))
-
-						if (ClassID == 5 and SubClassID == 1) or (ID == 180653) then
+						Class, SubClass = select(12, GetItemInfo(ID))
+						if (Class == 5 and SubClass == 1) or IDs[ID] then
 							return C_Container.UseContainerItem(bag, slot)
 						end
 					end
 				end
 			end
 		end)
-
 		if (not Frame:IsMovable()) then
 			Frame:SetMovable(true)
 			Frame:SetClampedToScreen(true)
@@ -32,7 +26,6 @@ KeystoneInserter:SetScript("OnEvent", function(self, event, addon)
 			Frame:SetScript("OnDragStart", Frame.StartMoving)
 			Frame:SetScript("OnDragStop", Frame.StopMovingOrSizing)
 		end
-
 		self:UnregisterEvent(event)
 	end
 end)
