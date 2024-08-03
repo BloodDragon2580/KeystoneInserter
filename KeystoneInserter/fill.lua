95eb382f6d9002e5cf16123f7c72186f6314c09b
BloodDragon2580 <gewinner.thomas@gmail.com>
2024-08-03 14:39:07 +0200

    Update for TWW

--- KeystoneInserter/fill.lua
+++ KeystoneInserter/fill.lua
@@ -64,7 +64,20 @@ local mapIdToActivity = {
     [406] = 1168, -- Halls of Infusion
     [463] = 1247, -- Dawn of the Infinite: Galakrond's Fall
     [464] = 1248, -- Dawn of the Infinite: Murozond's Rise
-	
+
+    -- The War Within
+    [500] = 2669, -- City of Threads
+    [501] = 2660, -- Ara-Kara, City of Echoes
+    [502] = 2652, -- Stonevault
+    [503] = 2662, -- Dawnbreaker
+    [504] = 2648, -- The Rookery
+    [505] = 2651, -- Darkflame Cleft
+    [506] = 2661, -- Cinderbrew Brewery
+    [507] = 2649, -- Priory of the Sacred Flame
+    [508] = 670,  -- Grim Batol
+    [509] = 1822, -- Seige of Boralus (Alliance)
+    [510] = 1822, -- Seige of Boralus (Horde)
+
 };
 
 local keystoneLink = '|cffa335ee|Hkeystone:%s:%s:%s:%s:%s:%s:%s|h' .. _G.CHALLENGE_MODE_KEYSTONE_HYPERLINK .. '|h|r';
